2498eebc312e3c7f1316098f3c01e9c84a318d5d
Maksym <139574751+MaxShmih@users.noreply.github.com>
2026-07-15 15:45:53 +0300

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+       const mapS = new Map();
+
+  if (s.length !== t.length) return false;
+
+  for (let i = 0; i < s.length; i++) {
+    mapS.set(s[i], (mapS.get(s[i]) ?? 0) + 1);
+  }
+
+  for (let char of t) {
+    if (mapS.get(char) > 0) {
+      mapS.set(char, mapS.get(char) - 1);
+    } else return false;
+  }
+
+  return true;
+    }
+}
