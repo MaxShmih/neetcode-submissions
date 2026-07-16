d4b488b3a2117029d0cbee11d6e01d4529926734
Maksym <139574751+MaxShmih@users.noreply.github.com>
2026-07-17 02:06:48 +0300

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+  const resMap = new Map();
+
+  for (const word of strs) {
+    const alfabet = new Array(26).fill(0);
+
+    for (const char of word) {
+      let index = char.charCodeAt(0) - "a".charCodeAt(0);
+      alfabet[index]++;
+    }
+    const key = alfabet.join("|");
+
+    if (!resMap.has(key)) {
+      resMap.set(key, []);
+    }
+
+    resMap.get(key).push(word);
+  }
+  return [...resMap.values()];
+}
+}
