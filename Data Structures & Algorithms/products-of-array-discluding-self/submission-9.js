3fcd6bc12e8867b9f38a8498d72cd3258d760fd8
Maksym <139574751+MaxShmih@users.noreply.github.com>
2026-07-22 17:18:01 +0300

    Add: products-of-array-discluding-self - submission-9

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-9.js
@@ -0,0 +1,19 @@
+class Solution {
+    productExceptSelf(nums) {
+       const resLeft = [];let leftProducts = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    resLeft.push(leftProducts);
+    leftProducts *= nums[i];
+  }
+
+  let rightProduct = 1;
+
+  for (let j = nums.length - 1; j >= 0; j--) {
+    resLeft[j] *= rightProduct;
+    rightProduct *= nums[j];
+  }
+
+  return resLeft;
+    }
+}
